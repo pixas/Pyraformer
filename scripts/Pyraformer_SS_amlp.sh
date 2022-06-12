687c13d0e44727a1a90c3ef7f37218af92ac0802
pixas <jiangshuyang@sjtu.edu.cn>
2022-06-12 23:33:18 +0800

    modify single step code

--- scripts/Pyraformer_SS_amlp.sh
+++ scripts/Pyraformer_SS_amlp.sh
@@ -1,3 +1,3 @@
-python single_step_main.py -data_path data/elect/ -dataset elect -attn_type amlp -amlp_dim 32 -amlp_fn relu;
-python single_step_main.py -data_path data/wind/ -dataset wind -attn_type amlp -amlp_dim 32 -amlp_fn relu;
-python single_step_main.py -data_path data/flow/ -dataset flow -attn_type amlp -amlp_dim 32 -amlp_fn relu;
+python single_step_main.py -data_path data/elect/ -dataset elect -enc_attn_type amlp -enc_amlp_dim 32 -enc_amlp_fn relu;
+python single_step_main.py -data_path data/wind/ -dataset wind -enc_attn_type amlp -enc_amlp_dim 32 -enc_amlp_fn relu;
+python single_step_main.py -data_path data/flow/ -dataset flow -enc_attn_type amlp -enc_amlp_dim 32 -enc_amlp_fn relu;

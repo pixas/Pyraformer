aed46b1ce17d8fb29264c8a1d79c3f19a3af3443
pixas <jiangshuyang@sjtu.edu.cn>
2022-06-14 14:10:11 +0800

    enlarge batch size

--- scripts/Pyraformer_SS.sh
+++ scripts/Pyraformer_SS.sh
@@ -1,3 +1,3 @@
-python single_step_main.py -data_path data/elect/ -dataset elect -save_path 'baseline';
-python single_step_main.py -data_path data/wind/ -dataset wind -save_path 'baseline';
-python single_step_main.py -data_path data/flow/ -dataset flow -save_path 'baseline';
+python single_step_main.py -data_path data/elect/ -dataset elect -save_path 'baseline' -inner_batch 32;
+python single_step_main.py -data_path data/wind/ -dataset wind -save_path 'baseline' -inner_batch 32;
+python single_step_main.py -data_path data/flow/ -dataset flow -save_path 'baseline' -inner_batch 32;

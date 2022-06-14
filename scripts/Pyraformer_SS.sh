70ba33d8c708d264b3b3d2beb2e510d3f7fa6cfd
pixas <jiangshuyang@sjtu.edu.cn>
2022-06-14 14:05:02 +0800

    add save path in training scripts

--- scripts/Pyraformer_SS.sh
+++ scripts/Pyraformer_SS.sh
@@ -1,3 +1,3 @@
-python single_step_main.py -data_path data/elect/ -dataset elect -eval;
-python single_step_main.py -data_path data/wind/ -dataset wind -eval;
-python single_step_main.py -data_path data/flow/ -dataset flow -eval;
+python single_step_main.py -data_path data/elect/ -dataset elect -save_path 'baseline';
+python single_step_main.py -data_path data/wind/ -dataset wind -save_path 'baseline';
+python single_step_main.py -data_path data/flow/ -dataset flow -save_path 'baseline';
--- scripts/Pyraformer_SS_amlp.sh
+++ scripts/Pyraformer_SS_amlp.sh
@@ -1,3 +1,3 @@
-python single_step_main.py -data_path data/elect/ -dataset elect -enc_attn_type amlp -enc_amlp_dim 32 -enc_amlp_fn relu;
-python single_step_main.py -data_path data/wind/ -dataset wind -enc_attn_type amlp -enc_amlp_dim 32 -enc_amlp_fn relu;
-python single_step_main.py -data_path data/flow/ -dataset flow -enc_attn_type amlp -enc_amlp_dim 32 -enc_amlp_fn relu;
+python single_step_main.py -data_path data/elect/ -dataset elect -enc_attn_type amlp -enc_amlp_dim 32 -enc_amlp_fn relu -save_path 'amlp_32';
+python single_step_main.py -data_path data/wind/ -dataset wind -enc_attn_type amlp -enc_amlp_dim 32 -enc_amlp_fn relu -save_path 'amlp_32';
+python single_step_main.py -data_path data/flow/ -dataset flow -enc_attn_type amlp -enc_amlp_dim 32 -enc_amlp_fn relu -save_path 'amlp_32';
